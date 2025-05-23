68355243b41d1bafd66060dfc10d603880557744
balajivinoth <146202696+balajivinoth@users.noreply.github.com>
2025-05-23 19:57:09 +0530

    Add files via upload

---
+++ testtoday/testtoday/Controllers/CustomerController.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using testtoday.model;
+
+namespace testtoday.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CustomerController : ControllerBase
+	{
+		private readonly string connectionstring;
+		public CustomerController(IConfiguration configuration) {
+			connectionstring = configuration.GetConnectionString("DefaultConnection")!;
+		}
+		[HttpPost]
+		public IActionResult Create(Customerdto customerdto)
+		{
+			try{
+				using(var connection = new SqlConnection(connectionstring))
+				{
+					Console.WriteLine("db connected");
+					connection.Open();
+					string sql = "Insert into Users(name,email)" +
+						"output Inserted.*"+
+						"values(@name,@email)";
+					var customer = new Customer()
+					{
+						name = customerdto.name,
+						email = customerdto.email
+					};
+					var newCustomer = connection.QuerySingleOrDefault<Customer>(sql,customer);
+					if(newCustomer != null)
+					{
+						return Ok(newCustomer);
+					}
+				}
+				
+
+
+			}catch (Exception ex) { 
+				Console.WriteLine("we have an exception"+ex.Message);
+			}	
+			return BadRequest();
+		}
+	}
+}
---
+++ testtoday/testtoday/model/Customerdto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace testtoday.model
+{
+	public class Customerdto
+	{
+		[Required]
+		public string name { get; set; }
+		[Required]
+		public string email { get; set; }
+	}
+}
